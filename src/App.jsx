3d34eee042bd742633b6d7fa9504af0c8d47558b
GiuntaGiovanni <jhonniest741@gmail.com>
2025-11-17 21:37:25 +0100

    feat add global context provider on main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Context
+import GlobalContextProvider from "./context/GlobalContextProvider";
 //Layouts
 import DefaultLayout from "./layouts/DefaultLayout";
 //Pages
@@ -7,16 +10,20 @@ import HomePage from "./pages/home/HomePage";
 import ScrollToTop from "./components/technical/scrollToTop/ScrollToTop";
 
 export default function App() {
-	return (
-		<BrowserRouter>
-			<ScrollToTop />
-			<Routes>
-				<Route path="/it/" element={<DefaultLayout />}>
-					<Route index element={<HomePage />} />
-				</Route>
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <GlobalContextProvider>
+        <Routes>
+          <Route path="/it/" element={<DefaultLayout />}>
+            <Route index element={<HomePage />} />
+          </Route>
 
-				<Route path="/eng/" element={<DefaultLayout />}></Route>
-			</Routes>
-		</BrowserRouter>
-	);
+          <Route path="/eng/" element={<DefaultLayout />}>
+            <Route index element={<HomePage />} />
+          </Route>
+        </Routes>
+      </GlobalContextProvider>
+    </BrowserRouter>
+  );
 }
