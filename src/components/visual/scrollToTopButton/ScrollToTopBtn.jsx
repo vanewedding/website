791e8fdd2d26fc48411ed0bcec6e1d9183c0c4a9
GiuntaGiovanni <jhonniest741@gmail.com>
2025-11-22 17:06:17 +0100

    fix: z-index inferiore all'header

--- src/components/visual/scrollToTopButton/ScrollToTopBtn.jsx
+++ src/components/visual/scrollToTopButton/ScrollToTopBtn.jsx
@@ -1,37 +1,38 @@
 // import Hooks
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 // import styles
-import styles from './ScrollToTopBtn.module.css';
+import styles from "./ScrollToTopBtn.module.css";
 
 export default function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
 
-    const [isVisible, setIsVisible] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            setIsVisible(window.scrollY > 200);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-        });
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsVisible(window.scrollY > 200);
     };
 
-    // RENDER
-    return (
-        <button
-            onClick={scrollToTop}
-            aria-label="Scroll to Top"
-            className={`${styles.scrollButton} ${isVisible ? styles.scrollButtonVisible : ''}`}
-        >
-            &#8593;
-        </button>
-    );
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  // RENDER
+  return (
+    <button
+      onClick={scrollToTop}
+      aria-label="Scroll to Top"
+      className={`${styles.scrollButton} ${
+        isVisible ? styles.scrollButtonVisible : ""
+      } z-99`}
+    >
+      &#8593;
+    </button>
+  );
 }
