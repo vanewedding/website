18aeaaafc9fa3ae3952740088529c94a696a1bb5
GiuntaGiovanni <jhonniest741@gmail.com>
2025-11-24 20:05:44 +0100

    feat add desktop data on footer data

--- src/data/footer.js
+++ src/data/footer.js
@@ -1,22 +1,29 @@
 import footer from "../assets/img/footer.jpg";
 
 export const footerData = {
-	title: {
-		it: "PROGETTIAMO IL TUO GIORNO",
-		eng: "LET'S DESIGN YOUR DAY",
-	},
-	line1: {
-		it: "Il primo passo è fissare un appuntamento.",
-		eng: "The first step is to schedule an appointment.",
-	},
-	line2: {
-		it: "Ma prima, rispondi ad alcune domande:",
-		eng: "But first, answer a few questions:",
-	},
-	image: footer,
-	buttonText: {
-		it: "SCRIVIMI LE TUE IDEE",
-		eng: "WRITE ME YOUR IDEAS",
-		url: "/form",
-	},
+  title: {
+    it: "PROGETTIAMO IL TUO GIORNO",
+    eng: "LET'S DESIGN YOUR DAY",
+  },
+  line1: {
+    it: "Il primo passo è fissare un appuntamento.",
+    eng: "The first step is to schedule an appointment.",
+  },
+  line2: {
+    mobile: {
+      it: "Ma prima, rispondi ad alcune domande:",
+      eng: "But first, answer a few questions:",
+    },
+    desktop: {
+      it: "Ma prima, ho preparato un questionario per te.\nRispondi ad alcune domande: mi aiuteranno a prepararmi meglio per il nostro primo incontro",
+      eng: "But first, I’ve prepared a form for you.\nAnswer a few questions: they will help me get ready for our first meeting.",
+    },
+  },
+
+  image: footer,
+  buttonText: {
+    it: "SCRIVIMI LE TUE IDEE",
+    eng: "WRITE ME YOUR IDEAS",
+    url: "/form",
+  },
 };
