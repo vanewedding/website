e6e91a1475b771ec950e645e4ec058e1b788d9cf
GiuntaGiovanni <jhonniest741@gmail.com>
2025-12-30 19:05:16 +0100

    fix: update homepage URL in package.json and adjust base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
-  //base: mode === "production" ? "/vanewedding.it/" : "/",
-  base: "/",
+  base: mode === "production" ? "/vanewedding.it/" : "/",
 }));
